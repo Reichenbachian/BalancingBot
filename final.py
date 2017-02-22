0bdbefb0cad911786c0573f3a4b6d51737d253e1
DonQuixoteGM <gmorona@gmail.com>
2017-02-22 15:23:58 -0500

    Update final.py

--- final.py
+++ final.py
@@ -1,26 +1,25 @@
 import pandas as pd
 import numpy as np
 import random
-#import matplotlib.pyplot as plt
 import serial
 import rospy
 import roslib
 from geometry_msgs.msg import Twist
 
+# initializing various in order to control the robot
 ser = serial.Serial('/dev/ttyACM0', 9600)
 rospy.init_node('goalieBot', anonymous=True)
 movement = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=5)
 r = rospy.Rate(50);
 
-
 class Learner:
     def __init__(self):
         num_bins = 10
-        self.num_states = 2
-        self.num_states = num_bins ** self.num_states
-        self.num_actions = 3
-        self.ACTIONS = np.linspace(-.05, .05, self.num_actions)
-        self.qmatrix = np.random.uniform(low=-1, high=1, size=(self.num_states, self.num_actions))
+        num_states = 2
+        num_states = num_bins ** num_states
+        num_actions = 100
+        self.ACTIONS = np.linspace(-1.5, 1.5, num_actions)
+        self.qmatrix = np.zeros((num_states, num_actions))
         self.epsilon = .6
         self.gamma = .9
         self.alpha = .2
@@ -28,24 +27,26 @@ class Learner:
         self.state = 0
         self.actionIndex = None
 
-    # def set_initial(self, state):
-    #     self.state = state
-    #     self.actionIndex = self.qmatrix[state].argsort()[-1]
-    #     return self.ACTIONS
+    def set_initial(self, state):
+        self.state = state
+        self.actionIndex = self.qmatrix[state].argsort()[-1]
+        return self.ACTIONS
 
     def getMove(self, newState, reward):
-        if random.random() < self.epsilon:
-            return random.randint(0,self.num_actions)
+        if random.random() < self.epsilon: # allows for exploration (chooses a random action)
+            return random.randint(0,num_actions)
         newActionIndex = self.qmatrix[newState].argsort()[-1]
         self.qmatrix[self.state, self.actionIndex] = (1 - self.alpha) * self.qmatrix[self.state, self.actionIndex] + \
                                                     self.alpha*(reward + self.gamma * self.qmatrix[newState, newActionIndex])
         self.actionIndex = newActionIndex
         self.state = newState
-        self.epsilon *= self.epsilonDecay
-        return self.ACTIONS[newActionIndex
+        self.epsilon *= self.epsilonDecay # decreases the need of randomness as the model becomes more trained
+        return self.ACTIONS[newActionIndex]
 
 def build_state(features):
-    return int("".join(map(lambda feature: str(int(feature)), features)))
+    a = int("".join(map(lambda feature: str(int(feature)), features)))
+    print("The action index is: ", a)
+    return a
 
 def to_bin(value, bins):
     return np.digitize(x=[value], bins=bins)[0]
@@ -57,7 +58,6 @@ def getAngle():
             a = int(ser.readline().decode().strip())
         except:
             print("Failed reading...Trying again...")
-    return a
 
 def makeMove(action):
     move_cmd = Twist()
@@ -76,7 +76,8 @@ def cart_pole_with_qlearning():
     previousAngle = getAngle()
 
     # we are not going to differentiate between episode and step because,
-    # when we train the robot, we let it run without differentiating between "episodes"
+    # when we train the robot, we let it run without ever stopping it
+    # at each episode (i.e. moment when robot fails)
     for step in range(10000):
         currentAngle = getAngle()
         state = build_state([to_bin(currentAngle, currentAngleBins),
@@ -87,7 +88,5 @@ def cart_pole_with_qlearning():
         makeMove(speed)
         previousAngle = currentAngle
 
-
 if __name__ == "__main__":
     cart_pole_with_qlearning()
-
